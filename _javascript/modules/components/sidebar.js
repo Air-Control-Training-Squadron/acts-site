7ba0f2ece8a17448054b3c3e187a38fd4609cfa7
Timmy Le <14254403+DaDerpDuck@users.noreply.github.com>
2023-05-31 17:29:36 -0700

    Add mobile drag gesture

--- _javascript/modules/components/sidebar.js
+++ _javascript/modules/components/sidebar.js
@@ -4,6 +4,8 @@
 
 const $body = $('body');
 const ATTR_DISPLAY = 'sidebar-display';
+const ATTR_DRAGGING = 'sidebar-dragging';
+const SIDEBAR_WIDTH = 260;
 
 class SidebarUtil {
   static isExpanded = false;
@@ -17,9 +19,60 @@ class SidebarUtil {
 
     SidebarUtil.isExpanded = !SidebarUtil.isExpanded;
   }
+
+  static expand() {
+    $body.attr(ATTR_DISPLAY, '');
+
+    SidebarUtil.isExpanded = true;
+  }
+
+  static collapse() {
+    $body.removeAttr(ATTR_DISPLAY);
+
+    SidebarUtil.isExpanded = false;
+  }
+
+  static enableDragging() {
+    $body.attr(ATTR_DRAGGING, '');
+  }
+
+  static disableDragging() {
+    $body.removeAttr(ATTR_DRAGGING);
+  }
 }
 
 export function sidebarExpand() {
   $('#sidebar-trigger').on('click', SidebarUtil.toggle);
   $('#mask').on('click', SidebarUtil.toggle);
+
+  let touchStartX = null;
+  let diffX = null;
+
+  document.documentElement.style.setProperty('--sidebar-offset', '0px');
+
+  $(document).on('touchstart', e => {
+    touchStartX = e.originalEvent.touches[0].clientX;
+    document.documentElement.style.setProperty('--sidebar-offset', '0px');
+    SidebarUtil.enableDragging();
+  });
+
+  $(document).on('touchmove', e => {
+    const touchMoveX = e.originalEvent.touches[0].clientX;
+    diffX = touchMoveX - touchStartX;
+
+    document.documentElement.style.setProperty('--sidebar-offset', `${diffX}px`);
+  });
+
+  $(document).on('touchend', () => {
+    document.documentElement.style.setProperty('--sidebar-offset', '0px');
+    SidebarUtil.disableDragging();
+
+    const threshold = SIDEBAR_WIDTH / 2;
+
+    if (diffX > threshold) {
+      SidebarUtil.expand();
+    } else if (diffX < -threshold) {
+      SidebarUtil.collapse();
+    }
+  });
 }
